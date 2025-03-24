925e354eb77a7ea32edb644982128e1051703d3a
MirkanPolat <Mirkanpolat@outlook.de>
2025-03-24 14:23:39 +0100

    update last standpoint

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+function renderAll(){
+  renderMenus();
+  renderBasket();
+}
 function renderMenus() { 
   let menuLeft = document.getElementById("menuList"); // rendert das menu
   menuLeft.innerHTML = "";
@@ -5,6 +9,8 @@ function renderMenus() {
   for (let i = 0; i < myDishes.length; i++) {
     menuLeft.innerHTML += renderMenuTemplate(i); // html wird returnt
   }
+}
+function renderBasket(){
   let basket = document.getElementById("basket");
   basket.innerHTML = "";
 
@@ -22,7 +28,7 @@ function renderMenuTemplate(i) { // rendert das linke menu
     <div class="dishes"> 
         <div class="dishes-header">
        <p>${myDishes[i].name}</p>
-       <img onclick="addDishes(${i})" src="/assets/img/Logo/plus.png" alt="Add Product" height="55px">
+       <img onclick="addDishes(${i})" src="assets/img/Logo/plus.png" alt="Add Product" height="55px">
         </div> 
        <p>${myDishes[i].description}</p>
        <p class="under-text" >${myDishes[i].price}</p>
@@ -40,7 +46,7 @@ function renderBasketTemplate(y){ // rendert den warenkorb
       <p>${myBasket[y].amount}</p>
       <p>+</p>
       <p>${myBasket[y].price}</p>
-      <img src="/assets/img/Logo/trash.png" alt="trash" height="35px" />
+      <img src="assets/img/Logo/trash.png" alt="trash" height="35px"/>
     </div>
   </div>
   <div>
@@ -53,6 +59,10 @@ function renderBasketTemplate(y){ // rendert den warenkorb
         <td>lieferkosten:</td>
         <td> 5.00€</td>
       </tr>
+      <tr>
+       <td><b>Gesamt</b></td>
+       <td><b>${(myBasket[y].price + 5.00)}</b></td>
+      </tr>
     </table>
   </div>
 </div>
@@ -60,25 +70,27 @@ function renderBasketTemplate(y){ // rendert den warenkorb
     `
 }
 function addDishes(i){ // pusht den menu ins basket
+
+  if(myBasket[i]){
+    myBasket[i].amount++
+  }else
     myBasket.push({
       name: myDishes[i].name,
       price: myDishes[i].price,
       amount: myDishes[i].amount
     });
     
-
     let basket = document.getElementById("basket");
     basket.innerHTML = "";
 
-  renderMenus();
-    
+    renderBasket();
 }
 function emptyBasket(){ // zeigt den leeren Warenkorb an 
     return /*html*/`
         <div>
             <div class="split"></div>
             <div class="content">
-              <p class="center">Wähle leckere Gerichte aus der Karte und bestelle dein Menu <img src="/assets/img/Logo/empty-basket.png" height="55px" alt="basket"></p>
+              <p class="center">Wähle leckere Gerichte aus der Karte und bestelle dein Menu <img src="assets/img/Logo/empty-basket.png" height="55px" alt="basket"></p>
             <div>
         </div>
     `
