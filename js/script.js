83eed01ee789b2babf479a33911f1005ea5dda2b
MirkanPolat <Mirkanpolat@outlook.de>
2025-03-27 19:18:41 +0100

    fixing some problems

--- js/script.js
+++ js/script.js
@@ -6,7 +6,6 @@ function renderAll(){
 function renderMenus() { 
   let menuLeft = document.getElementById("menuList"); // rendert das menu
   menuLeft.innerHTML = "";
-
   for (let i = 0; i < myDishes.length; i++) {
     menuLeft.innerHTML += renderMenuTemplate(i); // html wird returnt
   }
@@ -15,27 +14,21 @@ function renderMenus() {
 function renderBasket(){
   let basket = document.getElementById("basket")
   basket.innerHTML = "";
-  
-
   if(name.length === 0){
     basket.innerHTML = emptyBasket();
   }else{
-
- 
   for (let y = 0; y < name.length; y++) {
-   
     basket.innerHTML += renderBasketTemplate(y);
-
   }
 }
 }
 
-
 function deleteEVerything(y){ // deleteAll
-    amount.splice(y,1);
-    price.splice(y,1);
-    name.splice(y,1);
+  amount.splice(y,1);
+  price.splice(y,1);
+  name.splice(y,1);
   renderBasket();
+  calcDeliveryInfos();
 }
 
 function deleteOneMore(y){ // - delete one dish
@@ -47,28 +40,31 @@ function deleteOneMore(y){ // - delete one dish
     name.splice(y,1);
   }
   renderBasket();
+  calcDeliveryInfos();
+  renderDeliveryInfosTemplate();
 }
 
 function addOneMore(y){ // + amount
     amount[y]++;
     renderBasket();
+    calcDeliveryInfos();
 }
 
 function calcDeliveryInfos(){
   let delivery = document.getElementById("delivery-info");
   let subTotal = 0;
-  
   for (let x = 0; x < price.length; x++) {
     subTotal += price[x]*amount[x]
   }
-
   let deliveryCost = 5.00;
   let totalCost = subTotal + deliveryCost;
-
   if(price.length == 0){
-    delivery.innerHTML += "";
+    delivery.innerHTML = "";
+    calcButton(0);
   }else{
   delivery.innerHTML = renderDeliveryInfosTemplate(totalCost,deliveryCost,subTotal);
+  calcButton(totalCost);
+  renderDeliveryInfosTemplate();
   }
 }
 
@@ -78,7 +74,6 @@ function getMenuIndex(i){
 
 function addDishes(i){ // pusht den menu ins basket
   let indexes = getMenuIndex(i);
- 
   if(amount[indexes] > 0){
     amount[indexes]++
   }else
@@ -90,15 +85,11 @@ function addDishes(i){ // pusht den menu ins basket
     renderBasket();
     calcDeliveryInfos();
 }
+function stopWindow(event) {
+  event.stopPropagation();
+}
 
-function emptyBasket(){ // zeigt den leeren Warenkorb an 
-    return /*html*/`
-        <div>
-            <div class="split"></div>
-            <div class="content">
-              <p class="center">Wähle leckere Gerichte aus der Karte und bestelle dein Menu <img src="assets/img/Logo/empty-basket.png" height="55px" alt="basket"></p>
-            <div>
-        </div>
-    `
-    
+function calcButton(totalCost){
+  document.getElementById("overlayOnMenu").innerHTML = 
+  `Bestellen: ${totalCost.toFixed(2)} €`;
 }
--- js/template.js
+++ js/template.js
@@ -1,77 +1,90 @@
 function renderMenuTemplate(i) { // rendert das linke menu
   return /*html*/ `
-      <div class="dishes"> 
-          <div class="dishes-header">
-         <p>${myDishes[i].name}</p>
-         <img onclick="addDishes(${i})" src="assets/img/Logo/plus.png" alt="Add Product" height="55px">
-          </div> 
-         <p>${myDishes[i].description}</p>
-         <p class="under-text" >${myDishes[i].price.toFixed(2)} €</p>
-      </div>
-      `;
+<div class="dishes">
+  <div class="dishes-header">
+    <p>${myDishes[i].name}</p>
+    <img class="cursor"
+      onclick="addDishes(${i})"
+      src="assets/img/Logo/plus.png"
+      alt="Add Product"
+      height="55px">
+  </div>
+  <p>${myDishes[i].description}</p>
+  <p class="under-text">${myDishes[i].price.toFixed(2)} €</p>
+</div>
+`;
 }
 
-function renderBasketTemplate(y) { // rendert den warenkorb
+function renderBasketTemplate(y){ // rendert den warenkorb
   return /*html*/ `
-  <div class="content">
-    <p>${name[y]}</p>
-    <div class="amounts">
-      <p onclick="deleteOneMore(${y})">-</p>
-      <p>${amount[y]}</p>
-      <p onclick="addOneMore(${y})">+</p>
-      <p>${(price[y] * amount[y]).toFixed(2)} €</p>
-      <img onclick="deleteEVerything(${y})" src="assets/img/Logo/trash.png" alt="trash" height="35px"/>
-    </div>
-</div>
+<div class="content">
+  <p>${name[y]}</p>
+  <div class="amounts">
+    <p class="cursor" onclick="deleteOneMore(${y})">-</p>
+    <p>${amount[y]}</p>
+    <p class="cursor" onclick="addOneMore(${y})">+</p>
+    <p>${(price[y].toFixed(2) * amount[y]).toFixed(2)} €</p>
+    <img onclick="deleteEVerything(${y})"
+      src="assets/img/Logo/trash.png"
+      alt="trash"
+      height="35px">
   </div>
-  
+</div>
     `;
 }
 
 function renderDeliveryInfosTemplate(totalCost,deliveryCost,subTotal){ // In the building 
     return /*html*/`
-        <table>
-          <tr>
-            <td>Zwischensumme:</td>    
-            <td>${subTotal} €</td>
-          </tr>
-          <tr>
-            <td>lieferkosten:</td>
-            <td> ${deliveryCost} €</td>
-          </tr>
-          <tr>
-           <td><b>Gesamt</b></td>
-           <td><b>${totalCost} €</b></td>
-          </tr>
-        </table>
-    
-        <button onclick="openOverlay()" class="btn"> Bestellung Aufgeben TOTAL: ${totalCost}</button>
+<table>
+  <tr>
+    <td>Zwischensumme:</td>
+    <td>${subTotal.toFixed(2)} €</td>
+  </tr>
+  <tr>
+    <td>lieferkosten:</td>
+    <td>${deliveryCost.toFixed(2)} €</td>
+  </tr>
+  <tr>
+    <td><b>Gesamt</b></td>
+    <td><b>${totalCost.toFixed(2)} €</b></td>
+  </tr>
+</table>
+<button onclick="openOverlay()" class="btn">
+  Bestellen TOTAL: ${totalCost.toFixed(2)}
+</button>
     `
-    }
+}
 
 function openOverlay(y){
     document.getElementById("overlay").classList.toggle("d-none")
-    deleteEVerything(y)
+    deleteEVerything(y);
     finishedBlock();
 }
 
 function finishedBlock(){
-    document.getElementById("overlay").innerHTML = 
- /*html*/`
-   <div onclick="stopWindow(event)" class="job-done">
-    <button onclick="openOverlay()" class="btn">Close me</button>
-    <p class="center">Vielen Dank ,
-        ihre Bestellung wird in Ca. 40min. eintreffen.
-    </p>
-   </div>
+document.getElementById("overlay").innerHTML = 
+/*html*/`
+<div onclick="stopWindow(event)" class="job-done">
+  <button onclick="openOverlay()" class="btn">Close me</button>
+  <p class="center">
+    Vielen Dank , ihre Bestellung wird in Ca. 40min. eintreffen.
+  </p>
+</div>
     `
 }
-
-function stopWindow(event) {
-    event.stopPropagation();
-  }
 
 function openOverlayOnMenu(){
     document.getElementById("overlayMenu").classList.toggle("d-none");
     document.getElementById("toggle-rightPart").classList.toggle("hidden");
+}
+
+function emptyBasket(){ // zeigt den leeren Warenkorb an 
+  return /*html*/`
+      <div>
+          <div class="split"></div>
+          <div class="content">
+            <p class="center">Wähle leckere Gerichte aus der Karte und bestelle dein Menu <img src="assets/img/Logo/empty-basket.png" height="55px" alt="basket"></p>
+          <div>
+      </div>
+  `
 }
